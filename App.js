a510211d1ce0e963f21856d90ffd795a57bad995
RajaDommeti <rajadommeti158@gmail.com>
2023-12-23 15:31:10 +0530

    my commit

--- App.js
+++ App.js
@@ -8,6 +8,6 @@ import ReactDOM from "react-dom";
 
 const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root "));
 
 root.render(jsxHeading);
